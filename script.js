81e0923f3e8c6d3916c56bfe01a8cdd847bc287d
mauro-dev-site <maurodomingos1996@gmail.com>
2025-07-25 15:19:33 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-// Scroll suave ao clicar nos menus
+// 
 document.querySelectorAll('a[href^=\"#\"]').forEach(link => {
     link.addEventListener('click', function(e) {
       e.preventDefault();
       const target = document.querySelector(this.getAttribute('href'));
       if (target) {
         target.scrollIntoView({ behavior: 'smooth' });
+        // Futuras interações em JS (pode adicionar animações ou funcionalidades)
+console.log('Mauro Dev Site carregado com sucesso.');
+
       }
       emailjs.sendForm('service_f5b4mid', 'template_xxxxxx', this)
     });
